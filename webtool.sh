7729dba2d401b6c34d80d514b5c33fb92f4f373e
gg <gg@gmail.com>
2023-10-29 13:33:44 +0800

    change port

---
+++ webtool.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+stop(){
+pid=$(lsof -i:80|awk '{print $2}'|sed -n '2p')
+if [[ -z "${pid}" ]]
+then
+    echo application is not running
+else
+    echo kill "${pid}"
+    kill -9 "${pid}"
+fi
+}
+
+start(){
+nohup go run main.go >> out.log 2>&1 &
+echo application is running
+}
+
+cd /opt/webtool-go || exit
+git pull
+
+if [ "$1" == "stop" ]
+then
+    stop
+elif [ "$1" == "start" ]
+then
+    start
+elif [ "$1" == "restart" ]
+then
+    stop
+    start
+else
+    echo "incorrect args |start|stop|restart|"
+fi
+
