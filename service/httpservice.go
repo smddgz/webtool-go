a7ec1c0bbe532524e3f90c0ad8c42cb51d85d35b
gg <gg@gmail.com>
2023-10-29 13:40:58 +0800

    update

--- service/httpservice.go
+++ service/httpservice.go
@@ -6,7 +6,7 @@ func StartHttp() {
 	r := gin.Default()
 	r.GET("/ping", func(c *gin.Context) {
 		c.JSON(200, gin.H{
-			"message": "nmsl",
+			"message": "sbwy",
 		})
 	})
 	r.Run("0.0.0.0:80") // listen and serve on 0.0.0.0:8080
